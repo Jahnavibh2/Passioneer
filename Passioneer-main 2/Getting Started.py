f996ee286e1d86eed2f8956cb18f37b013cc2fc0
Jahnavibh2 <86475161+Jahnavibh2@users.noreply.github.com>
2022-11-06 23:46:32 -0500

    Add files via upload

---
+++ Passioneer-main 2/Getting Started.py
@@ -0,0 +1,12 @@
+import streamlit as st
+# import streamlit.components.v1 as components
+
+# with open('index.html') as f:
+#     render_html = f.read()
+    
+# components.html(render_html)
+
+st.title("Getting Started")
+st.text("Pick one of the activities from the sidebar")
+st.text("Drums | Guitar | Workout | Yoga")
+
